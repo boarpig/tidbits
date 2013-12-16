097bbd41a5d463879fe4349f3d07ca18ad4a5647
Lauri Hakko <lauri.hakko@gmail.com>
2013-12-16 21:12:09 +0200

    handled last line better

--- hexview.py
+++ hexview.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/python
 
 from sys import argv
 from os.path import isfile
@@ -11,9 +11,9 @@ if len(argv) == 2 and isfile(argv[1]):
         s = f.read()
     for i, item in enumerate(s):
         if (i+1)%16 == 0 and i != 0:
-            print("%s: " % (str(hex(i+1))[2:].zfill(8).upper()), end='')
+            print("{}: ".format(str(hex(i + 1 - 16))[2:].zfill(8).upper()), end='')
             eight.append(str(hex(item))[2:].zfill(2).upper())
-            print("%s: " % (' '.join(eight)), end='')
+            print("{}: ".format(' '.join(eight)), end='')
             for item in eight:
                 char = chr(int(item, 16))
                 if char in printable and char not in whitespace:
@@ -24,6 +24,10 @@ if len(argv) == 2 and isfile(argv[1]):
             eight = []
         else:
             eight.append(str(hex(item))[2:].zfill(2).upper())
+    # TODO: make this print the last line number correctly
+    print("{}: ".format(str(hex(i+1))[2:].zfill(8).upper()), end='')
+    print("{} ".format(' '.join(eight)), end='')
+    print("  " * (16 - len(eight)) + " " * (16 - len(eight) -1) + ": ", end='')
     for item in eight:
         char = chr(int(item, 16))
         if char in printable and char not in whitespace:
